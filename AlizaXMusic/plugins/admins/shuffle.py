60a8398e4cd334b45d143ebad7696713056ee225
LEGEND X <109813393+TeamLegend77@users.noreply.github.com>
2022-07-28 17:56:08 +0530

    🥀

--- AlizaXMusic/plugins/admins/shuffle.py
+++ AlizaXMusic/plugins/admins/shuffle.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import random
 
 from pyrogram import filters
@@ -14,9 +5,9 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import db
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from AlizaXMusic import app
+from AlizaXMusic.misc import db
+from AlizaXMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
