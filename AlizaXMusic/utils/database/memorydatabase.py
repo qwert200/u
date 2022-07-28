5252428c0c8e13b3e45c4d832aa23ed94c2c6636
LEGEND X <109813393+TeamLegend77@users.noreply.github.com>
2022-07-28 16:57:34 +0530

    🥀

--- AlizaXMusic/utils/database/memorydatabase.py
+++ AlizaXMusic/utils/database/memorydatabase.py
@@ -1,16 +1,7 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 
 import config
 from config import PRIVATE_BOT_MODE
-from YukkiMusic.core.mongo import mongodb
+from AlizaXMusic.core.mongo import mongodb
 
 channeldb = mongodb.cplaymode
 commanddb = mongodb.commands
