d172b0deb1ecb4c33731ffa188714a3555b6419e
LEGEND X <109813393+TeamLegend77@users.noreply.github.com>
2022-07-28 17:26:35 +0530

    🥀

--- AlizaXMusic/plugins/sudo/blacklistchat.py
+++ AlizaXMusic/plugins/sudo/blacklistchat.py
@@ -1,23 +1,15 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
 
 from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (blacklist_chat,
+from AlizaXMusic import app
+from AlizaXMusic.misc import SUDOERS
+from AlizaXMusic.utils.database import (blacklist_chat,
                                        blacklisted_chats,
                                        whitelist_chat)
-from YukkiMusic.utils.decorators.language import language
+from AlizaXMusic.utils.decorators.language import language
 
 # Commands
 
