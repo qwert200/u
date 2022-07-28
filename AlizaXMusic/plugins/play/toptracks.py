102008fc18fe837827f5159a907161d6c970a3df
LEGEND X <109813393+TeamLegend77@users.noreply.github.com>
2022-07-28 17:30:11 +0530

    🥀

--- AlizaXMusic/plugins/play/toptracks.py
+++ AlizaXMusic/plugins/play/toptracks.py
@@ -1,26 +1,17 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import asyncio
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup
 
 from config import BANNED_USERS
-from YukkiMusic import app
-from YukkiMusic.utils.database import (get_global_tops,
+from AlizaXMusic import app
+from AlizaXMusic.utils.database import (get_global_tops,
                                        get_particulars, get_userss)
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.inline.playlist import (botplaylist_markup,
+from AlizaXMusic.utils.decorators.language import languageCB
+from AlizaXMusic.utils.inline.playlist import (botplaylist_markup,
                                               failed_top_markup,
                                               top_play_markup)
-from YukkiMusic.utils.stream.stream import stream
+from AlizaXMusic.utils.stream.stream import stream
 
 loop = asyncio.get_running_loop()
 
