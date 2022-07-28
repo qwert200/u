741d633d2e40cca8a15c19a3a47e9621a435a04d
LEGEND X <109813393+TeamLegend77@users.noreply.github.com>
2022-07-28 16:41:48 +0530

    🥀

--- AlizaXMusic/utils/stream/stream.py
+++ AlizaXMusic/utils/stream/stream.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import os
 from random import randint
 from typing import Union
@@ -14,20 +5,20 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from YukkiMusic import Carbon, YouTube, app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.misc import db
-from YukkiMusic.utils.database import (add_active_chat,
+from AlizaXMusic import Carbon, YouTube, app
+from AlizaXMusic.core.call import Aliza
+from AlizaXMusic.misc import db
+from AlizaXMusic.utils.database import (add_active_chat,
                                        add_active_video_chat,
                                        is_active_chat,
                                        is_video_allowed, music_on)
-from YukkiMusic.utils.exceptions import AssistantErr
-from YukkiMusic.utils.inline.play import (stream_markup,
+from AlizaXMusic.utils.exceptions import AssistantErr
+from AlizaXMusic.utils.inline.play import (stream_markup,
                                           telegram_markup)
-from YukkiMusic.utils.inline.playlist import close_markup
-from YukkiMusic.utils.pastebin import Yukkibin
-from YukkiMusic.utils.stream.queue import put_queue, put_queue_index
-from YukkiMusic.utils.thumbnails import gen_thumb
+from AlizaXMusic.utils.inline.playlist import close_markup
+from AlizaXMusic.utils.pastebin import Alizabin
+from AlizaXMusic.utils.stream.queue import put_queue, put_queue_index
+from AlizaXMusic.utils.thumbnails import gen_thumb
 
 
 async def stream(
