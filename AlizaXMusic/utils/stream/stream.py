ac821af1bc36e3af4a4c1eb41274a4ed7c2a59b8
LEGEND X <109813393+TeamLegend77@users.noreply.github.com>
2022-07-29 12:20:19 +0530

    Update stream.py

--- AlizaXMusic/utils/stream/stream.py
+++ AlizaXMusic/utils/stream/stream.py
@@ -40,7 +40,7 @@ async def stream(
         if not await is_video_allowed(chat_id):
             raise AssistantErr(_["play_7"])
     if forceplay:
-        await Yukki.force_stop_stream(chat_id)
+        await Aliza.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['playlist_16']}\n\n"
         count = 0
@@ -89,7 +89,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_16"])
-                await Yukki.join_call(
+                await Aliza.join_call(
                     chat_id, original_chat_id, file_path, video=status
                 )
                 await put_queue(
@@ -120,7 +120,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await Yukkibin(msg)
+            link = await Alizabin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -170,7 +170,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Yukki.join_call(
+            await Aliza.join_call(
                 chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
@@ -224,7 +224,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Yukki.join_call(
+            await Aliza.join_call(
                 chat_id, original_chat_id, file_path, video=None
             )
             await put_queue(
@@ -278,7 +278,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Yukki.join_call(
+            await Aliza.join_call(
                 chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
@@ -339,7 +339,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await Yukki.join_call(
+            await Aliza.join_call(
                 chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
@@ -391,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Yukki.join_call(
+            await Aliza.join_call(
                 chat_id,
                 original_chat_id,
                 link,
